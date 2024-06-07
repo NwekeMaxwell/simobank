94222893ff7e07528858d9447a0dbf5af384fdb5
NwekeMaxwell <nwekemaxwell37@gmail.com>
2024-06-07 13:54:28 +0100

    feat: add basic components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,47 @@
-export default function RootLayout({
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+// import { getLoggedInUser } from "@/lib/actions/user.actions";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // const loggedIn = await getLoggedInUser();
+  const loggedIn = {
+    $id: "1234456578",
+    email: "example@gmail.com",
+    userId: "asdasdasd",
+    dwollaCustomerUrl: "string",
+    dwollaCustomerId: "stringid",
+    firstName: "John",
+    lastName: "Doe",
+    name: "John Doe",
+    address1: "Doe street",
+    city: "Joe city",
+    state: "Jon state",
+    postalCode: "12343",
+    dateOfBirth: "11/12/1222",
+    ssn: "12345",
+  };
+
+  if(!loggedIn) redirect('/sign-in')
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,121 @@
-import Image from "next/image";
+import HeaderBox from "@/components/HeaderBox";
+// import RecentTransactions from '@/components/RecentTransactions';
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+// import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+// import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-export default function Home() {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
+  const loggedIn = {
+    $id: "1234456578",
+    email: "example@gmail.com",
+    userId: "asdasdasd",
+    dwollaCustomerUrl: "string",
+    dwollaCustomerId: "stringid",
+    firstName: "John",
+    lastName: "Doe",
+    name: "John Doe",
+    address1: "Doe street",
+    city: "Joe city",
+    state: "Jon state",
+    postalCode: "12343",
+    dateOfBirth: "11/12/1222",
+    ssn: "12345",
+  };
+  const accounts = {
+    data: [
+      {
+        id: "string",
+        availableBalance: 23345,
+        currentBalance: 2324,
+        officialName: "string",
+        mask: "string",
+        institutionId: "string",
+        name: "string",
+        type: "string",
+        subtype: "string",
+        appwriteItemId: "string",
+        shareableId: "string",
+      },
+    ],
+    totalBanks: 1,
+    totalCurrentBalance: 9009099,
+  };
+  // const loggedIn = await getLoggedInUser();
+  // const accounts = await getAccounts({
+  //   userId: loggedIn.$id
+  // })
+
+  // if(!accounts) return;
+
+  const accountsData: any = [
+    {
+      id: "string",
+      availableBalance: 23345,
+      currentBalance: 2324,
+      officialName: "string",
+      mask: "string",
+      institutionId: "string",
+      name: "string",
+      type: "string",
+      subtype: "string",
+      appwriteItemId: "string",
+      shareableId: "string",
+      transactions: [],
+    },
+  ]; //accounts?.data;
+  // const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = {
+    id: "string",
+    availableBalance: 23345,
+    currentBalance: 2324,
+    officialName: "string",
+    mask: "string",
+    institutionId: "string",
+    name: "string",
+    type: "string",
+    subtype: "string",
+    appwriteItemId: "string",
+    shareableId: "string",
+    transactions: [],
+  };
+  // const account = await getAccount({ appwriteItemId });
+
   return (
-   <h1>Home</h1>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
+        </header>
+
+        {/* <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        /> */}
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
+    </section>
   );
-}
+};
+
+export default Home;
